520e3d756bd4620f976820a559baf6899c8f68b6
yinxuanh160214 <yinxuanh@gmail.com>
2017-06-10 10:12:35 +0800

    Chapter 8 finished

--- blog/article/views.py
+++ blog/article/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 
+from article.models import Article, Comment
 
 def article(request):
     '''
     Render the article page
     '''
-    return render(request, 'article/article.html')
+    articles = Article.objects.all()
+    itemList = []
+    for article in articles:
+        items = [article]
+        items.extend(list(Comment.objects.filter(article=article)))
+        itemList.append(items)
+    context = {'itemList':itemList}
+    return render(request, 'article/article.html', context)
