9c891561a61bad914b98b6cdf0ee44e41442ee56
yinxuanh160214 <yinxuanh@gmail.com>
2017-06-11 11:21:51 +0800

    Chapter 13 finished

--- blog/article/models.py
+++ blog/article/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Article(models.Model):
     title = models.CharField(max_length=128, unique=True)
     content = models.TextField()
     pubDateTime = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User)
 
     def __str__(self):
         return self.title
@@ -14,6 +16,7 @@ class Article(models.Model):
 
 class Comment(models.Model):
     article = models.ForeignKey(Article)
+    user = models.ForeignKey(User)
     content = models.CharField(max_length=128)
     pubDateTime = models.DateTimeField(auto_now_add=True)
 
--- blog/article/urls.py
+++ blog/article/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     url(r'^articleUpdate/(?P<articleId>[0-9]+)/$', views.articleUpdate, name='articleUpdate'),
     url(r'^articleDelete/(?P<articleId>[0-9]+)/$', views.articleDelete, name='articleDelete'),
     url(r'^articleSearch/$', views.articleSearch, name='articleSearch'),
+    url(r'^articleLike/(?P<articleId>[0-9]+)/$', views.articleLike, name='articleLike'),
+    url(r'^commentCreate/(?P<articleId>[0-9]+)/$', views.commentCreate, name='commentCreate'),
+    url(r'^commentUpdate/(?P<commentId>[0-9]+)/$', views.commentUpdate, name='commentUpdate'),
+    url(r'^commentDelete/(?P<commentId>[0-9]+)/$', views.commentDelete, name='commentDelete'),
 ]
